dbad3373aa08c466e83a3fe2fe06aa43906f9a29
mrbodja <mrbodja@gmail.com>
2019-05-23 11:46:29 +0300

    classes and tests created

---
+++ MyProj/ClassLibrary/Properties/MyMath.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProj
+{
+   public  class MyMath
+    {
+        public double Sum(int[] intArray)
+        {
+            double sum = 0;
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                sum += intArray[i];
+            }
+            return sum;
+        }
+        public double SerAryf(int []intArray)
+        {
+            double ser = 0;
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                ser += intArray[i];
+            }
+            return ser / intArray.Length;
+        }
+        static int MaxValue(int[] intArray)
+        {
+            int maxVal = intArray[0];
+            for (int i = 1; i < intArray.Length; i++)
+            {
+                if (intArray[i] > maxVal)
+                    maxVal = intArray[i];
+            }
+            return maxVal;
+        }
+    }
+}
---
+++ MyProj/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyProj;
+
+namespace UnitTestProject1
+{
+   
+    [TestClass]
+    public class UnitTest1
+    {
+        int[] array = new int[] { 1, 2, 3, 4, 5 };
+        [TestMethod]
+        public void TestSum()
+        {
+            MyMath myMath = new MyMath();
+            double actual = myMath.Sum( array);
+            Assert.AreEqual(15, actual);
+        }
+
+        [TestMethod]
+        public void TestMax()
+        {
+            MyMath myMath = new MyMath();
+            double actual = myMath.MaxValue(array);
+            Assert.AreEqual(5, actual);
+        }
+        [TestMethod]
+        public void TestAryf()
+        {
+            MyMath myMath = new MyMath();
+            double actual = myMath.SerAryf(array);
+            Assert.AreEqual(3, actual);
+        }
+    }
+
+
+}
+
+
